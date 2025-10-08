b8c47fa7f37e7f724668b8ad7a10f8a2f40e1263
Bruna Ramirez <ramirez.bruna29@gmail.com>
2025-10-08 14:23:47 +0000

    loops aninhados para cavalo

--- xadrez.c
+++ xadrez.c
@@ -5,16 +5,21 @@ int main(){
         //Bispo: 5 casas na diagonal superior direita
         //Torre: 5 casas para a direita
         //Rainha: 8 casas para a esquerda
+        //Cavalo: 2 casas para baixo e 1 casa para esquerda
 
         int casas_bispo = 5;
         int casas_torre = 5;
         int casas_rainha = 8;
+        int casas_baixo_cavalo = 2;
+        int casas_esquerda_cavalo = 1;
 
+        //Move o Bispo 5 casas para cima em diagonal
         printf(" --- Movimento do Bispo ---\n");
         for (int i = 1; i <= casas_bispo; i++) {
             printf("Diagonal cima direita\n");
         }
-
+        
+        //Move a Torre 5 casas para a direita
         printf(" --- Movimento da TORRE ---\n");
         int contadorTorre = 1;
         while (contadorTorre <= casas_torre) {
@@ -22,6 +27,7 @@ int main(){
         contadorTorre++;
         }
 
+        //Move a rainha 8 casas para a esquerda
         printf(" --- Movimento da Rainha ---\n");
         int contadorRainha = 1;
         do {
@@ -29,15 +35,16 @@ int main(){
             contadorRainha++;
         } while (contadorRainha <= casas_rainha);
 
+        //Move o Cavalho 2 casas para baixo e só após esse movimento ele move a peça para a esquerda
         printf(" --- Movimento do Cavalo ---\n");
-        for (int i = 1; i <= 2; i++) {
+        for (int i = 1; i <= casas_baixo_cavalo; i++) {
             printf("Baixo\n");
 
         }
-        int b = 1;
-        while (b <= 1) {
+        int j = 1;
+        while (j <= casas_esquerda_cavalo) {
             printf("Esquerda\n");
-            b++;
+            j++;
         }
 
 
@@ -45,5 +52,3 @@ int main(){
 
     return 0;
 }
-
-
